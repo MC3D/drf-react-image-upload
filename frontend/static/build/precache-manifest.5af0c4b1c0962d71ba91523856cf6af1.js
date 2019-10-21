271957cb236680f8ac645d544748b58abb9d839a
MC3D <m.chapman.home@gmail.com>
2019-10-21 15:42:58 -0400

    ran build for deployment

---
+++ frontend/static/build/precache-manifest.5af0c4b1c0962d71ba91523856cf6af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed4b4a82b99faa473cee86fa37c5d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d21e6ee8bca839750e4",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c008968d935310e98bfa",
+    "url": "/static/js/2.ac32026f.chunk.js"
+  },
+  {
+    "revision": "8d21e6ee8bca839750e4",
+    "url": "/static/js/main.8adf60d8.chunk.js"
+  },
+  {
+    "revision": "71dcedd2f2bbe7dcbd91",
+    "url": "/static/js/runtime-main.107b9db7.js"
+  }
+]);
